726ffc6e2866fbb890b2695efdfbc946abe436bb
lekesmile <lekesmile@hotmail.com>
2020-07-27 11:09:35 +0300

    adding chart

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -3,103 +3,87 @@ import { Line } from "react-chartjs-2";
 import numeral from "numeral";
 import Axios from "axios";
 
-const LineChart = ({ casesType = "cases" }) => {
-  const [data, setData] = useState({});
+const LineChart = () => {
+  // const [data, setData] = useState([]);
+  const [xAxis, setXaxix] = useState([]);
+  const [yAxis, setYaxix] = useState([]);
 
-  const options = {
-    legend: {
-      displace: false,
-    },
-    elements: {
-      point: {
-        radius: 0,
-      },
-    },
-    maintainAspectRatio: false,
-    tooltips: {
-      mode: "index",
-      intersect: false,
-      callbacks: {
-        label: function (tooltipItem, data) {
-          return numeral(tooltipItem.value).format("+0, 0");
-        },
-      },
-    },
-    scales: {
-      xAxes: [
-        {
-          type: "time",
-          time: {
-            format: "MM/DD/YY",
-            tooltipFormat: "ll",
-          },
-        },
-      ],
-      yAxes: [
-        {
-          fridLines: {
-            display: false,
-          },
-          ticks: {
-            callback: function (value, index, values) {
-              return numeral(value).format("0a");
-            },
-          },
-        },
-      ],
-    },
-  };
+  const fetchperdays = async () => {
+    const response = await Axios(
+      "https://disease.sh/v3/covid-19/historical/all?lastdays=120"
+    );
 
-  const buildChartData = (data, casesType = "cases") => {
-    const chartData = [];
-    let lastDataPoint;
-    for (data in data.cases) {
-      // data.cases.forEach((element) => {
-      if (lastDataPoint) {
-        const newData = {
-          x: data,
-          y: data["casesType"][data] - lastDataPoint,
-        };
-        chartData.push(newData);
-      }
-      lastDataPoint = data["casesType"][data];
+    for (const dataObj in response.data.cases) {
+      console.log(dataObj, response.data.cases[dataObj]);
+      setXaxix(dataObj);
+      setYaxix(response.data.cases[dataObj]);
     }
-    return chartData;
   };
 
+  const data1 = {
+    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    datasets: [
+      {
+        label: "My First dataset",
+        fill: true,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+    ],
+  };
+
+  const data = {
+    labels: numeral(xAxis),
+    datasets: [
+      {
+        label: "My First dataset",
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: "rgba(75,192,192,0.4)",
+        borderColor: "rgba(75,192,192,1)",
+        borderCapStyle: "butt",
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: "miter",
+        pointBorderColor: "rgba(75,192,192,1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: numeral(yAxis),
+      },
+    ],
+  };
+
+  console.log(data);
   useEffect(() => {
-    const fetchperdays = async () => {
-      const response = await Axios(
-        "https://disease.sh/v3/covid-19/historical/all?lastdays=120"
-      );
-
-      const builtData = buildChartData(response.data.cases);
-      setData(builtData);
-      console.log(response.data.cases);
-    };
-
     fetchperdays();
-  }, [casesType]);
+  }, []);
 
   return (
-    <div>
+    <div className="container">
       <h5>Last 120days </h5>
-      {data?.length > 0 && (
-        <Line
-          options={options}
-          width={80}
-          height={500}
-          data={{
-            datasets: [
-              {
-                backgroundColor: "rgba(204, 16, 52, 0.5)",
-                borderColor: "#cc1034",
-                data: data,
-              },
-            ],
-          }}
-        />
-      )}
+      {data?.length > 0 && <Line data={data} />}
+      <Line data={data1} />
     </div>
   );
 };
